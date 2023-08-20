2bf3a8f474d66b0d9007b04c01ad94943e43b252
FYLSen <FYLSen@users.noreply.github.com>
2023-08-20 18:44:07 +0800

    .

--- worker.js
+++ worker.js
@@ -3,35 +3,17 @@ addEventListener('fetch', event => {
 });
 
 async function getProxyList(event) {
-    const getReqHeader = (key) => event.request.headers.get(key);
-    
     const url = new URL(event.request.url);
     const path = 'https:/' + url.pathname;
 
-    let init = {
-        method: event.request.method,
-        headers: {
-            'User-Agent': getReqHeader("User-Agent"),
-            'Accept': getReqHeader("Accept"),
-            'Accept-Language': getReqHeader("Accept-Language"),
-            'Accept-Encoding': getReqHeader("Accept-Encoding"),
-            'Connection': 'keep-alive'
-        }
-    };
+        const modifiedRequest = new Request(path, event.request);
 
-    if (event.request.headers.has("Referer")) {
-        init.headers.Referer = getReqHeader("Referer");
-    }
-
-    if (event.request.headers.has("Origin")) {
-        init.headers.Origin = getReqHeader("Origin");
-    }
-    
-    const response = await fetch(path, event.request);
+    const response = await fetch(modifiedRequest);
     const results = await response.text();
     
-    var res = results.toString().match(/\[Proxy\]\n[\S\s]+\n\[Proxy Group\]/g);
-    var proxy_arr = res.toString().split(/[\[\]]/);
+    const proxyListRegex = /\[Proxy\]\n([\s\S]+?)\n\[Proxy Group\]/;
+    const match = proxyListRegex.exec(results);
+    const proxyList = match ? match[1] : '';
 
-    return new Response(proxy_arr[2].toString(), init)
+    return new Response(proxyList, response);
 }
